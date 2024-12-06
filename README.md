a5781fe1e7b5e3a4ebdc49640f0da900e5dbd0c2
unknown <aurorarose6425@gmail.com>
2024-12-06 17:32:17 -0500

    better than nothing

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Twitch-Directory-Cleaner
 Allows the user to have a list of streams to remove from category directories
+
+## How to install
+
+Get [Tampermonkey](https://www.tampermonkey.net/), [ViolentMonkey](https://violentmonkey.github.io/), or whatever you want to use to load userscripts.
+
+Then, click this link. Click install.
+
+Once you do, you can see it in action by going to the category of your choice.
+
+
+## Issues
+
+- Currently only works if you load the category directly. Will fix
+- Don't have all the channels listed. Either let me know on Discord (Artemis6425) or add them yourself by editing the script! Don't worry, I made it pretty straightforward.
---
+++ twitch-directory-cleaner.js
@@ -0,0 +1,86 @@
+// ==UserScript==
+// @name         Twitch Directory Cleaner
+// @namespace    https://github.com/Artemis6425/Twitch-Directory-Cleaner
+// @version      1.0
+// @updateURL
+// @downloadURL  
+// @description  Removes Channels from any directory if they are found on a list.
+// @author       Artemis6425
+// @match        *://*.twitch.tv/directory/category/*
+// @run-at       document-end
+// @grant        none
+// ==/UserScript==
+
+
+
+// The main flaw here is that this requires the category to be refreshed by the user. will be fixed
+
+(function() {
+    'use strict';
+    var streams = [
+      	"simplyspeedruns",
+      	"armada247",
+      	"bubzia247",
+      	"puncayshunstreams",
+      	"weegeewatchparty",
+      	"popewatchparty",
+      	"zfg247"
+    ]
+
+
+    const waitForContainer = () => {
+        const container = document.querySelector('div[data-target="directory-container"]');
+        if (container) {
+            console.log('Container found:', container);
+
+            // Deletes all streams on category load
+            Array.from(container.children).forEach((child, index) => {
+              	if(child.classList.contains('tw-tower')){
+                	if (child.children.length > 0) {
+                  		Array.from(child.children).forEach((nestedChild, nestedIndex) => {
+                    		if(nestedChild.hasAttribute('data-target')){
+                      			var links = nestedChild.querySelectorAll('a[href]')
+                      			for(i=0; i < streams.length; i++){
+                        			var matchingLink = Array.from(links).find(link => link.href.includes(streams[i]));
+                        			if(matchingLink){
+                          				console.log("removing stream " + streams[i] + " from directory")
+                          				nestedChild.remove()
+                        			}
+                      			}
+                    		}
+                  		});
+                	}
+              	}
+            });
+
+            // Deletes all streams when scrolling
+            const observer = new MutationObserver((mutationsList) => {
+                mutationsList.forEach((mutation) => {
+                    if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+                        mutation.addedNodes.forEach((node) => {
+                            if (node.nodeType === Node.ELEMENT_NODE) {
+                                if(node.hasAttribute('data-target')){
+                                  	var links = node.querySelectorAll('a[href]')
+                                  	for(i=0; i < streams.length; i++){
+                                    	var matchingLink = Array.from(links).find(link => link.href.includes(streams[i]));
+                                    	if(matchingLink){
+                                      		console.log("removing stream " + streams[i] + " from directory")
+                                      		node.remove()
+                                    	}
+                                  	}
+                                }
+                            }
+                        });
+                    }
+                });
+            });
+
+            observer.observe(container, { childList: true, subtree: true });
+        } else {
+            console.log('Container not found, retrying...');
+            setTimeout(waitForContainer, 500); // Retry after 500ms
+        }
+    };
+
+    waitForContainer();
+})();
